430cdf8a955d88ecfe8ecc4132b6e38f38d0b28e
josephgit45 <josephlee.45@gmail.com>
2015-11-19 11:22:48 -0800

    added heat map working on navbar

--- SHARP demo/js/dropdown.js
+++ SHARP demo/js/dropdown.js
@@ -1,65 +1,55 @@
 //default data column
-var dataCategoriesTitles = ["Document Life Cycle", "Document Type", "Log Event ID"];
-var dataCategoriesColumns = ["_c4", "_c6", "_c9"];
+var dataCategoriesTitles = ["Document Life Cycle", "Document Type", "Log Event ID","Map Key","Dates Graph", "Daily Time Graph","US Heat Map","Global Trends"];
+var dataCategoriesColumns = ["_c4", "_c6", "_c9", "_c15"];
 
 var timeCategoriesTitles = ["Dates Graph", "Daily Time Graph"];
+var geographicCategoriesTitles = ["US Heat Map","Global Trends"];
 
-var currentColumn = dataCategoriesColumns[0];
+var currentValue = dataCategoriesColumns[0];
 
 $( document ).ready(function() {
   initialize();
 });
 
 function initialize(){
-  var sel = document.getElementById('dataCategoriesDropdownPie');
+  var sel = document.getElementById('dataCategoriesDropdown');
   for(var i = 0; i < dataCategoriesTitles.length; i++) {
       var opt = document.createElement('option');
       opt.innerHTML = dataCategoriesTitles[i];
       opt.value = dataCategoriesColumns[i];
       sel.appendChild(opt);
   }
+  makePieChart(currentValue);
+  // makeRadarChart(currentColumn);
+  //makeDateChart();
+  //makeUSHeatMap();
+}
 
-  var sel = document.getElementById('dataCategoriesDropdownRadar');
-  for(var i = 0; i < dataCategoriesTitles.length; i++) {
-      var opt = document.createElement('option');
-      opt.innerHTML = dataCategoriesTitles[i];
-      opt.value = dataCategoriesColumns[i];
-      sel.appendChild(opt);
+
+function getSelectedData(){
+  var data = document.getElementById("dataCategoriesDropdown");
+  currentValue = data.options[data.selectedIndex].value;
+  currentText =  data.options[data.selectedIndex].text;
+  console.log(currentValue + " " + currentText);
+  if(isInArray(currentValue,dataCategoriesColumns)){
+      makePieChart(currentValue);
   }
-
-  var sel = document.getElementById('timeCategoriesDropdown');
-  for(var i = 0; i < timeCategoriesTitles.length; i++) {
-      var opt = document.createElement('option');
-      opt.innerHTML = timeCategoriesTitles[i];
-      opt.value = timeCategoriesTitles[i];
-      sel.appendChild(opt);
-  }
-  makePieChart(currentColumn);
-  makeRadarChart(currentColumn);
-  makeDateChart();
-}
-
-
-function getSelectedDataPie(){
-  var data = document.getElementById("dataCategoriesDropdownPie");
-  currentColumn = data.options[data.selectedIndex].value;
-
-  makePieChart(currentColumn);
-}
-
-function getSelectedDataRadar(){
-  var data = document.getElementById("dataCategoriesDropdownRadar");
-  currentColumn = data.options[data.selectedIndex].value;
-
-  makeRadarChart(currentColumn);
-}
-
-function getSelectedTime(){
-  var e = document.getElementById("timeCategoriesDropdown");
-  if (e.options[e.selectedIndex].text == "Dates Graph"){
+  else if (currentText == "Dates Graph"){
     makeDateChart();
   }
-  else if (e.options[e.selectedIndex].text == "Daily Time Graph"){
+  else if (currentText == "Daily Time Graph"){
     makeTimeChart();
   }
+  else if (currentText == "Global Trends"){
+    makeGlobalTrendsMap();
+  }
+  else if (currentText == "US Heat Map"){
+    makeUSHeatMap();
+  }
+
+}
+
+//helper function
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
 }
--- SHARP demo/js/makeChart.js
+++ SHARP demo/js/makeChart.js
@@ -6,7 +6,7 @@
   function makePieChart(column){
     // document.write("<h1>SHARP CPO Data Analytics </br> Column:" + column + "</h1>");
 
-    pieChart = AmCharts.makeChart("piechartdiv", {
+    pieChart = AmCharts.makeChart("chartdiv", {
       "type": "pie",
       "startDuration": 0,
        "theme": "light",
@@ -68,7 +68,7 @@
   }
 
   function makeRadarChart(column){
-    var radarChart = AmCharts.makeChart( "radarchartdiv", {
+    var radarChart = AmCharts.makeChart( "chartdiv", {
       "type": "radar",
       "theme": "light",
       "dataProvider": parseJSON(column,false),
@@ -86,7 +86,7 @@
   }
 
 function makeDateChart(){
-  var dateChart = AmCharts.makeChart("timechartdiv", {
+  var dateChart = AmCharts.makeChart("chartdiv", {
     "type": "serial",
     "theme": "light",
     "marginRight": 80,
@@ -154,9 +154,9 @@ dateChart.addListener("rendered", zoomChart);
 }
 
 
-function makeTimeChart(column){
+function makeTimeChart(){
 
-var timeChart = AmCharts.makeChart("timechartdiv", {
+var timeChart = AmCharts.makeChart("chartdiv", {
     "type": "serial",
     "theme": "light",
     "marginRight": 80,
@@ -203,6 +203,221 @@ timeChart.addListener("dataUpdated", zoomChart);
 // we need to call zoomChart here
 // this method is called when chart is first inited as we listen for "dataUpdated" event
   // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-    timeChart.zoomToIndexes(chartData.length - 250, chartData.length - 100);
+    //timeChart.zoomToIndexes(chartData.length - 250, chartData.length - 100);
+}
 
+function makeUSHeatMap(){
+  var heatmap = AmCharts.makeChart( "chartdiv", {
+  type: "map",
+  "theme": "none",
+
+  colorSteps: 10,
+
+  dataProvider: {
+    map: "usaLow",
+    areas: [ {
+      id: "US-AL",
+      value: 4447100
+    }, {
+      id: "US-AK",
+      value: 623932
+    }, {
+      id: "US-AZ",
+      value: 5130632
+    }, {
+      id: "US-AR",
+      value: 2673400
+    }, {
+      id: "US-CA",
+      value: 33871648
+    }, {
+      id: "US-CO",
+      value: 4301261
+    }, {
+      id: "US-CT",
+      value: 3405565
+    }, {
+      id: "US-DE",
+      value: 783600
+    }, {
+      id: "US-FL",
+      value: 15982378
+    }, {
+      id: "US-GA",
+      value: 8186453
+    }, {
+      id: "US-HI",
+      value: 1211537
+    }, {
+      id: "US-ID",
+      value: 1293953
+    }, {
+      id: "US-IL",
+      value: 12419293
+    }, {
+      id: "US-IN",
+      value: 6080485
+    }, {
+      id: "US-IA",
+      value: 2926324
+    }, {
+      id: "US-KS",
+      value: 2688418
+    }, {
+      id: "US-KY",
+      value: 4041769
+    }, {
+      id: "US-LA",
+      value: 4468976
+    }, {
+      id: "US-ME",
+      value: 1274923
+    }, {
+      id: "US-MD",
+      value: 5296486
+    }, {
+      id: "US-MA",
+      value: 6349097
+    }, {
+      id: "US-MI",
+      value: 9938444
+    }, {
+      id: "US-MN",
+      value: 4919479
+    }, {
+      id: "US-MS",
+      value: 2844658
+    }, {
+      id: "US-MO",
+      value: 5595211
+    }, {
+      id: "US-MT",
+      value: 902195
+    }, {
+      id: "US-NE",
+      value: 1711263
+    }, {
+      id: "US-NV",
+      value: 1998257
+    }, {
+      id: "US-NH",
+      value: 1235786
+    }, {
+      id: "US-NJ",
+      value: 8414350
+    }, {
+      id: "US-NM",
+      value: 1819046
+    }, {
+      id: "US-NY",
+      value: 18976457
+    }, {
+      id: "US-NC",
+      value: 8049313
+    }, {
+      id: "US-ND",
+      value: 642200
+    }, {
+      id: "US-OH",
+      value: 11353140
+    }, {
+      id: "US-OK",
+      value: 3450654
+    }, {
+      id: "US-OR",
+      value: 3421399
+    }, {
+      id: "US-PA",
+      value: 12281054
+    }, {
+      id: "US-RI",
+      value: 1048319
+    }, {
+      id: "US-SC",
+      value: 4012012
+    }, {
+      id: "US-SD",
+      value: 754844
+    }, {
+      id: "US-TN",
+      value: 5689283
+    }, {
+      id: "US-TX",
+      value: 20851820
+    }, {
+      id: "US-UT",
+      value: 2233169
+    }, {
+      id: "US-VT",
+      value: 608827
+    }, {
+      id: "US-VA",
+      value: 7078515
+    }, {
+      id: "US-WA",
+      value: 5894121
+    }, {
+      id: "US-WV",
+      value: 1808344
+    }, {
+      id: "US-WI",
+      value: 5363675
+    }, {
+      id: "US-WY",
+      value: 493782
+    } ]
+  },
+
+  areasSettings: {
+    autoZoom: true
+  },
+
+  valueLegend: {
+    right: 10,
+    minValue: "little",
+    maxValue: "a lot!"
+  },
+
+  "export": {
+    "enabled": true
+  }
+
+} );
+}
+
+function makeGlobalTrendsMap(){
+  // create AmMap object
+  var map = new AmCharts.AmMap();
+  // set path to images
+  map.pathToImages = "ammap/images/";
+
+  /* create data provider object
+   map property is usually the same as the name of the map file.
+
+   getAreasFromMap indicates that amMap should read all the areas available
+   in the map data and treat them as they are included in your data provider.
+   in case you don't set it to true, all the areas except listed in data
+   provider will be treated as unlisted.
+  */
+  var dataProvider = {
+      map: "worldLow",
+      getAreasFromMap:true
+  };
+  // pass data provider to the map object
+  map.dataProvider = dataProvider;
+
+  /* create areas settings
+   * autoZoom set to true means that the map will zoom-in when clicked on the area
+   * selectedColor indicates color of the clicked area.
+   */
+  map.areasSettings = {
+      autoZoom: true,
+      selectedColor: "#CC0000"
+  };
+
+  // let's say we want a small map to be displayed, so let's create it
+  map.smallMap = new AmCharts.SmallMap();
+
+  // write the map to container div
+  map.write("chartdiv");
 }
